b6fa2cd78437dd5d43a5d635bee1e8128c8bbd00
masao <masao>
2020-06-14 14:07:11 +0900

    no message

--- 1-1/main.js
+++ 1-1/main.js
@@ -1,1 +1,20 @@
-console.log("Hello world")
+let array = [];
+
+for (let i = 2; i < process.argv.length; i++) {
+  array.push(Number(process.argv[i]));
+}
+
+let result = [];
+let len = array.length
+
+for(let i = 0; i < len; i++){
+  console.log(array.length)
+  let mini = array.reduce((a,b)=>a<b?a:b)
+   result.push(mini)
+  let num =  array.indexOf( mini, 0 );
+  array.splice( num, 1 );
+  
+}
+
+console.log(result)
+
